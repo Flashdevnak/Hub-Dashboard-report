d25d2b82a906b6f765f3e7e4cf42f83740be881a
Flashdevnak <26nak.testdev@gmail.com>
2026-03-18 16:56:19 +0700

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import { Suspense } from "react";
 import { LoginForm } from "@/components/login-form";
 
-export default function LoginPage() {
+export const dynamic = "force-dynamic";
+
+function LoginPageContent() {
   return <LoginForm />;
+}
+
+export default function LoginPage() {
+  return (
+    <Suspense fallback={<div className="p-6">Loading...</div>}>
+      <LoginPageContent />
+    </Suspense>
+  );
 }
